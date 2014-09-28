b8b51dbcf8d1b6126892556626f6c91984489546
Honlan <493722771@qq.com>
2014-09-28 17:10:08 +0800

    ckanext-mytheme added

---
+++ ckanext/mytheme/plugin.py
@@ -0,0 +1,25 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+class MyThemePlugin(plugins.SingletonPlugin):
+    '''An example theme plugin.
+
+    '''
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config):
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        toolkit.add_template_directory(config, 'templates')
+
+	# Add this plugin's public dir to CKAN's extra_public_paths, so
+        # that CKAN will use this plugin's custom static files.
+        toolkit.add_public_directory(config, 'public')
+	
+	# Register this plugin's fanstatic directory with CKAN.
+        # Here, 'fanstatic' is the path to the fanstatic directory
+        # (relative to this plugin.py file), and 'example_theme' is the name
+        # that we'll use to refer to this fanstatic directory from CKAN
+        # templates.
+        toolkit.add_resource('fanstatic', 'myresource')
